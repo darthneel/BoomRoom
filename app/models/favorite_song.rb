b0d6b4b86350310c62304aa52314801143c880d2
Neelp22 <neelp22@gmail.com>
2013-11-08 17:35:25 -0500

    added models

---
+++ app/models/favorite_song.rb
@@ -0,0 +1,4 @@
+class FavoriteSong < ActiveRecord::Base
+			has_and_belongs_to_many :users
+
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,22 +11,56 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131108145637) do
+ActiveRecord::Schema.define(version: 20131108194158) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "favorite_songs", force: true do |t|
+    t.string   "artist"
+    t.string   "title"
+    t.string   "sc_link"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "favorite_songs_users", id: false, force: true do |t|
+    t.integer  "favorite_song_id"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "rooms", force: true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "songs", force: true do |t|
+    t.string   "artist"
+    t.string   "title"
+    t.string   "sc_link"
+    t.text     "album_art"
+    t.boolean  "played",     default: false
+    t.integer  "room_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
   create_table "users", force: true do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
+    t.string   "email",                  default: "",    null: false
+    t.string   "encrypted_password",     default: "",    null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
+    t.integer  "sign_in_count",          default: 0,     null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
+    t.integer  "room_id"
+    t.boolean  "played",                 default: false
     t.datetime "created_at"
     t.datetime "updated_at"
   end
