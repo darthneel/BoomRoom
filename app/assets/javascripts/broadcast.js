d6cd5d074ae16dbb4f61f612c21feb7c915ec66a
MitulP91 <mitulp91@gmail.com>
2013-11-11 14:47:43 -0500

    make separate redis sub channels

--- app/assets/javascripts/broadcast.js
+++ app/assets/javascripts/broadcast.js
@@ -2,17 +2,24 @@ $(document).ready(function() {
   var source = new EventSource('/rooms/events');
   var room;
 
-  source.addEventListener('rooms.add_song', function (e) {
+  source.addEventListener(room_id + '.add_song', function (e) {
   	console.log('add song redis triggered');
   	data = JSON.parse(e.data);
   	console.log(data);
   	$("#room-" + data.room_id + " #playlist").append($('<li>').text(data.title));
   });
 
-  source.addEventListener('rooms.add_user', function (e) {
+  source.addEventListener(room_id + '.add_user', function (e) {
   	console.log('add user redis triggered');
     data = JSON.parse(e.data);
     console.log(data);
     $("#room-" + data.room_id + " #current-users").append($('<li>').text(data.user));
+  });
+
+  source.addEventListener(room_id + '.change_song', function (e) {
+  	console.log('change song redis triggered');
+  	data = JSON.parse(e.data);
+  	console.log(data);
+  	window.playSong(data.sc_ident);
   });
 });
