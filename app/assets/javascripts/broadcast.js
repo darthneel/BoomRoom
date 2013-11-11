62f98d139effcce1be16baea62e40dc54f882268
MitulP91 <mitulp91@gmail.com>
2013-11-10 19:29:04 -0500

    comment out turbolinks

--- app/assets/javascripts/broadcast.js
+++ app/assets/javascripts/broadcast.js
@@ -3,15 +3,16 @@ $(document).ready(function() {
   var room;
 
   source.addEventListener('rooms.add_song', function (e) {
-  	console.log('event triggered');
+  	console.log('add song redis triggered');
   	data = JSON.parse(e.data);
   	console.log(data);
-  	$("#playlist").append($('<li>').text(data.title));
+  	$("#room-" + data.room_id + " #playlist").append($('<li>').text(data.title));
   });
 
-  // source.addEventListener('rooms.add_user', function (e) {
-  //   data = JSON.parse(e.data);
-  //   console.log(data);
-  //   $("#" + data.room_id + " #user-list").append($('<li>').text(data.username));
-  // });
+  source.addEventListener('rooms.add_user', function (e) {
+  	console.log('add user redis triggered');
+    data = JSON.parse(e.data);
+    console.log(data);
+    $("#" + data.room_id + " #current-users").append($('<li>').text(data.user));
+  });
 });
