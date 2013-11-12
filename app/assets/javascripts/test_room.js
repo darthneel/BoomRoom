7759ede2fd4b2c7044299870bd065c07d4b76ffd
MitulP91 <mitulp91@gmail.com>
2013-11-12 10:41:31 -0500

    ec2 test

--- app/assets/javascripts/test_room.js
+++ app/assets/javascripts/test_room.js
@@ -79,9 +79,9 @@ $(function() {
 			genre = search_return[index].genre;
 		}
 		app.addNewSong(artist, title, stream_url, album_art, sc_ident, genre);
-		if (typeof(song) === 'undefined'){
-			playSong(sc_ident);
-		}
+
+		playSong(sc_ident);
+
 	});
 
 //** Media buttons 
